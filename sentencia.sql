43f470dac9195460f7e4c61999f84a249580c921
bianca <biancajoanalopez@gmail.com>
2024-03-26 22:40:42 -0300

    Ejercicios con SELEC,WHERE,DISTINCT,JOIN,ORDER BY

--- sentencia.sql
+++ sentencia.sql
@@ -1,2 +1,24 @@
 select * from inhabitant
-sadas
+
+SELECT name,Composer,Milliseconds FROM TRACKS
+
+SELECT name,Composer,Milliseconds FROM TRACKS
+
+SELECT name,Milliseconds FROM tracks 
+WHERE Composer IS NULL AND Milliseconds > 2900000
+
+SELECT LastName,FirstName, Company FROM customers WHERE Company IS NOT NULL
+
+SELECT DISTINCT BillingCity FROM invoices WHERE BillingState IS NOT NULL
+ORDER BY BillingCity DESC
+
+SELECT Title FROM albums 
+WHERE Title LIKE "%OF%"
+ORDER BY Title ASC
+
+SELECT t.name , A.Title FROM tracks t
+JOIN albums A ON t.AlbumId = A.AlbumId
+ORDER BY A.Title DESC
+
+SELECT t.name, g.name FROM tracks t
+JOIN genres g ON t.GenreId = g.GenreId
